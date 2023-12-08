0715fd1e977902f9f27d56e39bacec7993f1f79e
Tomer Kedem <tompha1@hotmail.com>
2023-12-08 20:15:23 +0200

    Query

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -3,16 +3,17 @@ using System.Text.Json.Serialization;
 
 public class Query
 {
-    public List<Book> Books => ReadBooks();
+    //public List<Book> Books => ReadBooks();
     public List<Magazine> Magazines => ReadMagazines();
     public List<IReadingMaterials> ReadingMaterials => GetReadingMaterials();
     public List<IThings> Things => GetThings();
 
-    private List<Book> ReadBooks()
+    public List<Book> Books(string nameContains = "")
     {
         string fileName = "Database/books.json";
         string jsonString = File.ReadAllText(fileName);
-        return JsonSerializer.Deserialize<List<Book>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter() } })!;
+        var books =  JsonSerializer.Deserialize<List<Book>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter() } })!;
+        return books.Where(b => b.Name.IndexOf(nameContains) >=0).ToList();
     }
 
     private List<Magazine> ReadMagazines()
@@ -24,14 +25,14 @@ public class Query
 
     private List<IReadingMaterials> GetReadingMaterials()
     {
-        var materials = ReadBooks().Cast<IReadingMaterials>().ToList();
+        var materials = Books().Cast<IReadingMaterials>().ToList();
         materials.AddRange(ReadMagazines().Cast<IReadingMaterials>().ToList());
         return materials;
     }
 
     private List<IThings> GetThings()
     {
-        var things = ReadBooks().Cast<IThings>().ToList();
+        var things = Books().Cast<IThings>().ToList();
         things.AddRange(ReadMagazines().Cast<IThings>().ToList());
         return things;
     }
